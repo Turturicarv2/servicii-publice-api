a21e5ca30aedb1f4001d4c327c04d3ae49aaa08f
Daniel PLAMADEALA [ MINDSOFT ] <danielp@indsoft.ro>
2025-08-27 12:16:44 +0300

    created ghiseu migration

---
+++ Models/Ghiseu.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiciiPubliceBackend.Models
+{
+    public class Ghiseu
+    {
+        public int Id { get; set; }
+        public string Cod { get; set; } = string.Empty;
+        public string Denumire { get; set; } = string.Empty;
+        public string Descriere { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public bool Activ {  get; set; } = true;
+    }
+}
---
+++ Repositories/GhiseuRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ServiciiPubliceBackend.Repositories
+{
+    public class GhiseuRepository
+    {
+        private readonly SqlConnection _sqlConnection;
+
+        public GhiseuRepository(SqlConnection sqlConnection)
+        {
+            _sqlConnection = sqlConnection;
+        }
+    }
+}
