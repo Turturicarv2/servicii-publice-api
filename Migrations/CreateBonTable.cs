f9207c664bba6e54dd5598bb41a739841dc43359
Daniel PLAMADEALA [ MINDSOFT ] <danielp@indsoft.ro>
2025-08-28 10:01:51 +0300

    remove dll files
    remove dll files from repo and add bon table migration

---
+++ Migrations/CreateBonTable.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+
+namespace ServiciiPubliceBackend.Migrations
+{
+    [Migration(202508270003)]
+    public class CreateBonTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Bon")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("IdGhiseu").AsInt32().NotNullable()
+                .WithColumn("Stare").AsString(20).NotNullable()
+                .WithColumn("CreatedAt").AsDateTime().NotNullable()
+                .WithColumn("ModifiedAt").AsDateTime();
+
+            Create.ForeignKey("FK_Orders_Ghiseu")
+                .FromTable("Bon").ForeignColumn("IdGhiseu")
+                .ToTable("Ghiseu").PrimaryColumn("Id")
+                .OnDeleteOrUpdate(System.Data.Rule.Cascade);
+
+            Execute.Sql(@"
+                ALTER TABLE Bon
+                ADD CONSTRAINT CK_Bon_Stare
+                CHECK (Stare IN ('in asteptare', 'preluat', 'inchis'));
+            ");
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Orders_Ghiseu").OnTable("Orders");
+            Delete.Table("Orders");
+        }
+    }
+}
