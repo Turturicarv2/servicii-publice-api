88c221e218aaedf5e55f77acb2fb8a43fb52a992
Daniel Plamadeala <danielp@indsoft.ro>
2025-09-11 11:36:28 +0300

    added migration for users table and added users model

---
+++ Migrations/CreateUsersTable.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace ServiciiPubliceBackend.Migrations
+{
+    [Migration(202509110004)]
+    public class CreateUsersTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Username").AsString().NotNullable()
+                .WithColumn("Password").AsString().NotNullable()
+                .WithColumn("Role").AsString().NotNullable();
+
+            Alter.Table("Bon")
+                .AddColumn("UserId").AsInt32().NotNullable();
+
+            Create.ForeignKey("FK_Users_Bon")
+                .FromTable("Bon").ForeignColumn("UserId")
+                .ToTable("Users").PrimaryColumn("Id")
+                .OnDeleteOrUpdate(System.Data.Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Users_Bon").OnTable("Users");
+            Delete.Column("UserId").FromTable("Bon");
+            Delete.Table("Users");
+        }
+    }
+}
